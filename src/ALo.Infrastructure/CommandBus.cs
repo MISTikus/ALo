49a892eb4903495c1270ab4572992f6b0593dc03
MTimoshinin <lord.mistikus@gmail.com>
2019-04-06 00:12:09 +0200

    feat: add simple CommandBus realization

---
+++ src/ALo.Infrastructure/CommandBus.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ALo.Infrastructure
+{
+    public interface ICommand
+    {
+    }
+    public interface ICommandHander<TCommand> where TCommand : ICommand
+    {
+        void Handle(TCommand command);
+    }
+    public interface ICommandBus
+    {
+        void Send(ICommand command);
+    }
+    public class CommandBus : ICommandBus
+    {
+        private readonly IResolver resolver;
+
+        public CommandBus(IResolver resolver) => this.resolver = resolver;
+
+        public void Send(ICommand command)
+        {
+            var type = command.GetType();
+            Trace.WriteLine($"Sending command '{type.Name}'");
+
+            var handlerInterface = typeof(ICommandHander<>);
+            var generic = handlerInterface.MakeGenericType(type);
+            var handle = generic.GetMethod(nameof(ICommandHander<ICommand>.Handle));
+
+            var handler = this.resolver.Get(generic);
+            if (handler == null)
+                throw new NotImplementedException($"Handler not found for command '{type.Name}'");
+            handle.Invoke(handler, new[] { command });
+        }
+    }
+}
---
+++ tests/ALo.Infrastructure.Tests/CommandBusTests.cs
@@ -0,0 +1,56 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Moq;
+using System;
+
+namespace ALo.Infrastructure.Tests
+{
+    public class CommandBusTests
+    {
+        private readonly Fixture random;
+
+        public CommandBusTests() => this.random = new Fixture();
+
+        [Xunit.Fact]
+        public void CommandBus_Shuld_Send_Command_To_Registered_Handler()
+        {
+            // Arrange
+            SampleCommand actual = null;
+            var handler = new Mock<ICommandHander<SampleCommand>>();
+            handler.Setup(x => x.Handle(It.IsAny<SampleCommand>())).Callback<SampleCommand>(c => actual = c);
+
+            var resolver = new Mock<IResolver>();
+            resolver.Setup(x => x.Get(typeof(ICommandHander<SampleCommand>))).Returns(handler.Object);
+
+            var expected = this.random.Create<SampleCommand>();
+            var bus = new CommandBus(resolver.Object);
+
+            // Action
+            bus.Send(expected);
+
+            // Assert
+            actual.Should().NotBeNull();
+            actual.Should().BeSameAs(expected);
+        }
+
+        [Xunit.Fact]
+        public void CommandBus_Should_Throw_NotImplementedException_If_Handler_Not_Found()
+        {
+            // Arrange
+            var resolver = new Mock<IResolver>();
+
+            var command = this.random.Create<SampleCommand>();
+            var bus = new CommandBus(resolver.Object);
+            void action(CommandBus c) => c.Send(command);
+
+            // Action && Assert
+            bus.Invoking(action).Should()
+                .Throw<NotImplementedException>()
+                .WithMessage($"Handler not found for command '{command.GetType().Name}'");
+        }
+
+        public class SampleCommand : ICommand
+        {
+        }
+    }
+}
